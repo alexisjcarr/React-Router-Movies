a44497ba3d592236df0b08eeca0fd596c33c5657
alexisjcarr <acarr59@gmail.com>
2019-06-10 16:24:50 -0500

    Note to self: Finish stretch tomorrow

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,16 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
+
+
+const StyledLink = styled(Link)`
+    text-decoration: none;
+    color: black;
+
+    &:focus, &:hover, &:visited, &:link, &:active {
+        text-decoration: none;
+    }
+`
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -13,9 +24,9 @@ export default class SavedList extends Component {
         {this.props.list.map(movie => (
           <span className="saved-movie">{movie.title}</span>
         ))}
-        <Link to="/">
+        <StyledLink to="/">
           <div className="home-button">Home</div>
-        </Link>
+        </StyledLink>
       </div>
     );
   }
